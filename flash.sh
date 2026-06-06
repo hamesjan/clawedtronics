d760d7bdc084efc57a009a546c506bb71fd63f95
hamesjan <jhan31903@gmail.com>
2026-06-06 13:43:43 -0700

    moving on to v2

--- flash.sh
+++ flash.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd ~/esp/esp-idf && . ./export.sh
 cd ~/projects/clawedtronics/hello_world
-idf.py -p /dev/cu.usbmodem101 flash monitor
+idf.py -p /dev/cu.usbmodem101 flash
+echo ""
+echo "Flash done. Unplug and replug USB, then run ./monitor.sh"
